7c772ab7a4e59b5821c96a68dedae25317058edb
tacosalfornoh <alexbrugamer@gmailcom>
2025-03-18 02:04:25 +0100

    finally added Discord OAuth redirect URI; add TODO for future Discord guild selection feature

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -8,3 +8,8 @@ pub fn home_page() -> View {
         <p> "We are excited to share more with you soon!" </p>
     }
 }
+
+// ! @todo Nella home poi che ci sará da selezionare i discords/gilde si puó vedere in quale discord una persona é dentro grazie all'api 
+// ! https://discord.com/api/users/@me/guilds 
+// ! Puoi vedere tutti i discord nel quale la persona é dentro poi guardi
+// ! se anche il bot é all'interno ed é abilitato e allora aggiungilo alla lista dei discords da selezionare
